106f0bc72e69b9ee6ac1b03aca73b737c647d089
Manasa T N <65770680+Manasa18tn@users.noreply.github.com>
2021-03-10 19:01:29 +0530

    day 1 assignment

---
+++ assignment 1/divby3.py
@@ -0,0 +1,32 @@
+lis=[]
+result=[]
+n=int(input("enter how many numbers need to check with divisability by 3 \n"))
+print("enter numbers")
+
+
+for i in range(0,n):
+      ele=int(input())
+      lis.append(ele)
+for r in lis:
+     if(r%3==0):
+          result.append(r)
+print(result)
+          
+
+           
+                
+
+                
+          
+      
+                
+     
+         
+                
+     
+          
+               
+               
+          
+          
+
---
+++ assignment 1/pow.py
@@ -0,0 +1,5 @@
+import math
+x=int(input("enter the value of x :"))
+y=int(input("enter the value of y :"))
+result=math.pow(x,y)
+print(result)
